47e3e4e011af5efb5136f5b390231578401ae568
Pablo Acebedo <pablofelipe@me.com>
2024-05-02 08:21:44 -0500

    Pricing

--- src/constants/index.js
+++ src/constants/index.js
@@ -243,6 +243,19 @@ export const pricing = [
     linkUrl: "https://example.com/gold"
   },
   
+  {
+    id: "2",
+    title: "Corporate",
+    description: "1 Año",
+    price: "Soon",
+    features: [
+      "100.000 Metros cuadrados",
+      // "Personalized recommendations based on your preferences",
+      // "Ability to explore the app and its features without any cost",
+    ],
+    linkUrl: "https://example.com/gold"
+  },
+  
 ];
 
 
